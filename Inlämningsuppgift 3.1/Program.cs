caee6a1f76df84a2e93b2dc9345db11325a511ad
ErnstOsbeck <143172129+ErnstOsbeck@users.noreply.github.com>
2023-10-04 10:26:35 +0200

    Add project files.

---
+++ Inlämningsuppgift 3.1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace uppgift_3._1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+            switch (ålder)
+            {
+                case < 16:
+                    {
+                        Console.WriteLine("Du är tyvärr för ung för att delta");
+                        break;
+                    }
+                case 16:
+                    {
+                        Console.WriteLine("Du får vara med och delta!");
+                        break;
+                    }
+                case 17:
+                    {
+                        Console.WriteLine("Du får vara med och delta!");
+                        break;
+                    }
+                case 18:
+                    {
+                        Console.WriteLine("Du får vara med och delta!");
+                        break;
+                    }
+                case 19:
+                    {
+                        Console.WriteLine("Du får vara med och delta!");
+                        break;
+                    }
+                case > 19:
+                    {
+                        Console.WriteLine("Du är tyvärr för gammal för att delta");
+                        break;
+                    }
+            }
+        }
+    }
+}
